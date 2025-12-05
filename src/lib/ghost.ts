e8a74b873da24037ad9fa65d6b4d8ccfc5eb73d0
mdreame <1067774901@qq.com>
2025-12-05 20:36:29 +0800

    fix: ghost https

--- src/lib/ghost.ts
+++ src/lib/ghost.ts
@@ -2,7 +2,7 @@ import GhostContentAPI from '@tryghost/content-api';
 
 // 使用站点凭证创建 API 实例
 export const ghostClient = new GhostContentAPI({
-    url: 'http://ghost.mdreame.life', // 这是 Ghost 站点运行在本地环境中的默认 URL
+    url: 'https://ghost.mdreame.life', // 这是 Ghost 站点运行在本地环境中的默认 URL
     key: import.meta.env.CONTENT_API_KEY,
     version: 'v5.0',
 });
